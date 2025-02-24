0d2fcafca7dc665b305a3a4640ab8f2c96f04324
mrcolor-blind <mrcolor-blind@users.noreply.github.com>
2025-02-24 09:06:03 -0600

    Substract update

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ function calculate() {
     document.getElementById("result").innerText = "Result: " + result;
 }
 
+function subs() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    let result = Number(num1) - Number(num2);
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
 function resetFields(){
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
-    document.getElementById("operator").value = "+";
+    //document.getElementById("operator").value = "+";
     document.getElementById("result").innerText = "Result: ";
 
 }
