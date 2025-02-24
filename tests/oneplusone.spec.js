74f0fab6dd7113b33b12b273fa4a7680a2835d02
mrcolor-blind <179273586+mrcolor-blind@users.noreply.github.com>
2025-02-24 08:03:27 -0600

    Update oneplusone.spec.js

--- tests/oneplusone.spec.js
+++ tests/oneplusone.spec.js
@@ -27,7 +27,7 @@ describe('-1+-1', function () {
                 .replace(/[^a-z0-9]/gi, '_')
                 .toLowerCase();
             const encodedString = await driver.takeScreenshot();
-            await fs.writeFileSync(./screenshots/${filename}.png, encodedString, 'base64');
+            await fs.writeFileSync(`./screenshots/${filename}.png`, encodedString, 'base64');
             await driver.quit();
         }
     });
