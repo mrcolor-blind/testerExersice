29904368850bbc470b2bef6fd4ce6fcb16032175
mrcolor-blind <179273586+mrcolor-blind@users.noreply.github.com>
2025-02-24 08:19:38 -0600

    Update oneplusone.spec.js

--- tests/oneplusone.spec.js
+++ tests/oneplusone.spec.js
@@ -43,6 +43,5 @@ describe('-1+-1', function () {
     await driver.findElement(By.id("num2")).click()
     await driver.findElement(By.id("num2")).sendKeys("1")
     await driver.findElement(By.css("button:nth-child(1)")).click()
-    await driver.close()
   })
 })
